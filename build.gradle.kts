9ad2240a223b5c5d4e39fa602fb3268fa7d2a8f7
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-08-28 13:22:22 +0200

    Update project version and dependency
    Incremented project version from 1.0.2.1 to 1.0.2.2. Updated io.github.breninsul:http-logging-commons dependency from 1.0.6 to 1.0.9 to incorporate the latest fixes and improvements.

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ val javaVersion = JavaVersion.VERSION_17
 val springBootVersion = "3.3.1"
 
 group = "io.github.breninsul"
-version = "1.0.2.1"
+version = "1.0.2.2"
 
 java {
     sourceCompatibility = javaVersion
@@ -62,7 +62,7 @@ tasks.compileKotlin {
 dependencies {
     compileOnly("org.springframework.boot:spring-boot-starter:$springBootVersion")
     compileOnly("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
-    api("io.github.breninsul:http-logging-commons:1.0.6")
+    api("io.github.breninsul:http-logging-commons:1.0.9")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
