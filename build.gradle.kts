e37d2796b307244d565c93cfe7022f8bf41a9f33
BreninSul <42633769+BreninSul@users.noreply.github.com>
2025-02-05 23:27:18 +0100

    Update dependency version and remove redundant HTTP header logic
    Upgraded the spring-input-stream-response-converter dependency to version 1.0.2. Simplified the HTTP message converter by removing redundant "Transfer-Encoding: chunked" header logic. This improves maintainability and aligns with updated library functionality.

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ val javaVersion = JavaVersion.VERSION_17
 val springBootVersion = "3.4.2"
 
 group = "io.github.breninsul"
-version = "1.0.1"
+version = "1.0.2"
 
 java {
     sourceCompatibility = javaVersion
